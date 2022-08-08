8ccd7f9ea02118d338356798444372322c59691c
Kamil Kałuża <kamil321.94@gmail.com>
2022-08-08 07:26:44 +0200

    character count

---
+++ characret_count.py
@@ -0,0 +1,12 @@
+from msilib.schema import LockPermissions
+
+
+message = 'Był jasny, zimny dzień kwietniowy i zegary biły trzynastą.'
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+print(count)
+
