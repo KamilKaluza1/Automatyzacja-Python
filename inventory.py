e5c9ffb7e643d507a6d8fd1cfa5d5794b9ba29d7
Kamil Kałuża <kamil321.94@gmail.com>
2022-08-08 10:19:16 +0200

    update inventory

--- inventory.py
+++ inventory.py
@@ -1,5 +1,7 @@
 inventory = {'lina': 1, 'pochodnia': 6, 'złote monety': 42, 'sztylet': 1, 'strzała': 12}
 
+dragonLoot = ['złote monety', 'sztylet', 'złote monety', 'złote monety', 'ruby']
+
 def displayInwentory(inventory):
     print('inwentarz: ')
     item_total = 0 
@@ -8,4 +10,13 @@ def displayInwentory(inventory):
         item_total = item_total + v
     print(f"w sumie {item_total} przedmiotów")
 
-displayInwentory(inventory)
+
+def add_to_inventory(inventory, dragonLoot):
+    for value in dragonLoot:
+        inventory.setdefault(value, 0)
+        inventory[value] = inventory[value] +1
+    displayInwentory(inventory)
+
+
+
+add_to_inventory(inventory, dragonLoot)
