20b7648ddf383f3a8fb5711f7827bea415f637a4
Kamil Kałuża <kamil321.94@gmail.com>
2022-08-14 10:14:59 +0200

    find_regex done

--- exercises/find_regex.py
+++ exercises/find_regex.py
@@ -1,11 +1,25 @@
 import re, os
-# Otwiera i przeszukuje wszystkie pliki txt znajdujące się w bieżącym katalogu
+# Utwórz program otwierający wszystkie pliki typu .txt znajdujące się w katalogu
+# oraz szukający wierszy dopasowanych do podanego przez użytkownika wyrażenia
+# regularnego. Wynik działania programu powinien być wyświetlony na ekranie.
+
+# Otwiera i przeszukuje wszystkie pliki txt znajdujące się w bieżącym katalogu OKOKOK
 # Szuka wyrażenia regularnego podanego przez użytkownika 
 # Wyświetla liczbę odnalezionych wyrażeń regularnych 
-# re.compile(r'[A]')
 
+item = input("czego szukać?")
 
+is_Kansas = re.compile(str(item))
+is_txt= re.compile(r'(txt)$')
+fund = 0
 
 
 for file in os.listdir('katalog'):
-    open((os.path.abspath(str(file)), 'r')
+    if is_txt.search(file) != None:
+        path = os.path.join(os.getcwd(), "katalog", file)
+        fileOpen = open(path, 'r')
+        fileContent = fileOpen.read()
+        i = is_Kansas.findall(fileContent)
+        fund += len(i)
+
+print(f"znaleziono {fund} wyników wyszukiwania")
