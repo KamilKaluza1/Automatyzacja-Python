eeba4e4f2ce4dec4cb84d6e08e759b8d99cbc758
Kamil Kałuża <kamil321.94@gmail.com>
2022-08-19 09:30:01 +0200

    sel1.0

---
+++ exercises/kopiowanie_selektywne.py
@@ -0,0 +1,10 @@
+# Przechodzenie przez drzewo katalogu wyszukiwanie plików o określonym rozszerzeniu .jpg
+# znalezione pliki wklejamy do nowego katalogu znalezione 
+import os, shutil
+katalog = 'C:\\Users\\48533\\OneDrive\\Pulpit\Automatyzacja nudnych zadań python\\exercises\\katalog'
+znalezione = 'C:\\Users\\48533\\OneDrive\\Pulpit\\Automatyzacja nudnych zadań python\\exercises\\znalezione'
+for folderName, subfolders, filenames in os.walk(katalog):
+    for filename in filenames:
+        if filename.endswith(".jpg"):
+            path = os.path.realpath(filename)
+            shutil.move(path, znalezione)
