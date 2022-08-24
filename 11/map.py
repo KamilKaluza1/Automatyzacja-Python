fb877a860fbf71298e136d10614e036f05bcf760
Kamil Kałuża <kamil321.94@gmail.com>
2022-08-24 07:45:47 +0200

    map - done

--- 11/map.py
+++ 11/map.py
@@ -1,6 +1,11 @@
 #! python3
+# Wywietla w przeglądarce mape na podstawie adersu podanego w wierszu poleceń lub schowku
 
-for value in range(5):
-    print(value)
+import webbrowser, sys, pyperclip
 
-input("podaj coś ")
+if len(sys.argv) > 1:
+    adress = ' '.join(sys.argv[1:]) # pobranie adresu z cmd
+else:
+    adress = pyperclip.paste()
+
+webbrowser.open(f'https://www.google.pl/maps/place/{adress}')
