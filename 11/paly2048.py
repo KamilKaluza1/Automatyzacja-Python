793137ad5c5a94a41c452772a1a5a9411f8cdbd7
Kamil Kałuża <kamil321.94@gmail.com>
2022-08-25 16:35:30 +0200

    infinite player -done

---
+++ 11/paly2048.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.get('https://play2048.co/')
+htmlElem = driver.find_element(By.TAG_NAME, "html")
+score = driver.find_element(By.CLASS_NAME, "score-container")
+
+while True:
+    htmlElem.send_keys(Keys.ARROW_UP)
+    htmlElem.send_keys(Keys.ARROW_RIGHT)
+    htmlElem.send_keys(Keys.ARROW_DOWN)
+    htmlElem.send_keys(Keys.ARROW_LEFT)
+    
+    try:
+        agine = driver.find_element(By.CLASS_NAME, "retry-button")
+        agine.click()
+    except:
+        continue
+#pomysł 
